26b529816fe35a1d4eb8099ac8826236b3fb6474
ReaverAmv <reaverzx@ptct.net>
2025-12-31 11:45:26 +0100

    First Commit

---
+++ Code/Player/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+class_name Player
+
+enum STATE {
+	FALL,
+	FLOOR,
+	JUMP,
+	DOUBLE_JUMP,
+	LEDGE_HANG,
+	LEDGE_CLIMB,
+	SWORD_ATTACK,
+	SHURIKEN_ATTACK,
+	SPECIAL_ATTACK,
+	AIR_ATTACK,
+	DASH,
+	WALL_SLIDE,
+	WALL_JUMP,
+}
+
+var can_dash: bool = false
+var can_special_attack: bool = false
+var can_shuriken_attack: bool = false
+var can_double_jump: bool = false
+var can_wall_jump: bool = false
+
+var active_state: STATE = STATE.FALL
+var previous_state: STATE = active_state
+
+const GRAVITY: float = 1500.0
+const FALL_SPEED: float = 640.0
+
+@export_category("References")
+@export_group("Essentials")
+@export var animator: AnimationPlayer
+@export var collider: CollisionShape2D
+
+func _ready() -> void:
+	switch_state(active_state)
+	
+func _physics_process(delta: float) -> void:
+	process_state(delta)
+	move_and_slide()
+
+func switch_state(to_state: STATE) -> void:
+	active_state = to_state
+	match active_state:
+		STATE.FALL:
+			animator.play("jump_fall")
+
+func process_state(delta: float) -> void:
+	match active_state:
+		STATE.FALL:
+			pass
